c716efd7d94c4711ac5149241d3de57e34bb3029
Isabella Vieira <isabella.q.vieira@gmail.com>
2022-07-14 20:45:52 -0400

    added search bar styling

--- immersive-game-review/src/Components/SearchBar.js
+++ immersive-game-review/src/Components/SearchBar.js
@@ -1,10 +1,27 @@
+import styled from "styled-components"
+
+const Searchs = styled.div`
+    margin: 0 0 10px 0;
+
+    .search{
+        background-color: #ae9;
+        height: 25px;
+        width: 250px;
+    } 
+
+
+`
 
 
 function Searchbar({handleingtheSearch}){
 
     return(
-        <input type="text" placeholder="Search by Name and Genre"
+        <Searchs>
+            <input type="text"className="search" placeholder="Search by Name and Genre..."
                 onChange={(synthEvent)=> handleingtheSearch(synthEvent.target.value)}/>
+
+        </Searchs>
+    
     )
 }
 export default Searchbar
